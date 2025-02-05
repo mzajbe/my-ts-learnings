33b419adefeba750eb0d1e59633de2e88de1fe36
mzajbe <mzajbe@gmail.com>
2025-02-05 16:40:08 +0600

    spread-and-rest

---
+++ module/src/basic/spread-and-rest-operator/spread.ts
@@ -0,0 +1,15 @@
+
+//array
+const bros1 : string[] = [
+    "zajbe",
+    "Mohammad",
+    "Jobayer"
+];
+
+const bros2 : string[] = [
+    "yusuf",
+    "Ali",
+    "Omar"
+]
+
+bros1.push(...bros2);
